89b8939a20d30f0cd85b643e98108462687a5a6e
Jeremy Oden <jerem.oden@gmail.com>
2021-08-14 17:21:13 +0200

    First commit.

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body columns">
+        <div class="container">
+          <h1 class="title">
+            eXaDrums News
+          </h1>
+          <h2 class="subtitle">
+            Latest news about eXaDrums
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <Nuxt />
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
